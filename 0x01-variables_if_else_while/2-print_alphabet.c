97f2d30ae37dcb62309c16f17dc851f8218651b4
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-11 14:57:31 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: print alphabet
+ *
+ * Return : 0 (success)
+*/
+int main(void)
+{
+	char ch = "a";
+
+	while (ch <= "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

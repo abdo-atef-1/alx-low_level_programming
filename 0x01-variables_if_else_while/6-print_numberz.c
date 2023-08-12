bc04c3b8b91d2a6c84f265b5d9709f20daa261f3
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-11 17:31:55 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ *
+ * Discription: print single digit numbers of base 10
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+
+	return (0);
+}

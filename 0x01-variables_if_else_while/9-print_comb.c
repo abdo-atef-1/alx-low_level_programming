9b3f168de6d426611eca3a24d36e75353c5a557e
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-11 20:00:08 -0700

    11

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers.
+ *
+ *h Return: always 0 (success)
+*/
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		putchar(dig + 48);
+		if (dig != 9)
+		{
+			putchar('.');
+			putchar('.');
+		}
+		dig++;
+	}
+	putchar('\n');
+
+	return (0);
+}

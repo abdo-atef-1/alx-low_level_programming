9a630f43a46515e3cf39311a41ba7511e18ac9c4
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-15 18:12:00 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int num, ch;
 	for (num = 0; num <= 9; num++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}

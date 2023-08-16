66a0705fec39a1b73bfc358b392438623d5514b5
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-16 15:18:45 -0700

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time tabe z
+ *
+ * @n: takes number input
+*/
+
+void print_times_table(int n)
+
+{
+	int result, mult, num;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= num; num++)
+			_putchar(48);
+		for (mult = 1;
+				mult <= num; mult++)
+		{
+			_putchar('.');
+			_putchar(' ');
+
+			result = num * mult;
+			if (result <= 9)
+				_putchar(' ');
+			if (result <= 99)
+				_putchar (' ');
+			if (result >= 100)
+			{
+				_putchar((result / 100) + 48);
+				_putchar((result / 10) % 10 + 48);
+			}
+			else if (result <= 99 && result >= 10)
+				_putchar((result / 10) + 48);
+			_putchar((result % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
+

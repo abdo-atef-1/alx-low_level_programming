4d2c2ce14b36f33d703a9e3207377b8aebef8898
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-16 06:13:55 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  jack_bauer - prints all minutes of the day
+*/
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(":");
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar("\n");
+		}
+	}

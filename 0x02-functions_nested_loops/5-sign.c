16b786831beccb3a2e7f3890f92876b47c3c730d
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-15 19:57:56 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_sign - print + if n is greater than zero.
+ * print_sign - iprint + if n is greater than zero.
  *		0 if n is zero and - if n is less
  *		than zero.
  *@n: takes integer type input for function.
- * Return1 if +. 0 if 0 and -1 if -
+ * Return: 1 if +. 0 if 0 and -1 if -
 */
 int print_sign(int n)
 {

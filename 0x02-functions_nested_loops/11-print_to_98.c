c029d449ff5c2e4c6614828ee55f0cf46db42150
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-16 11:31:16 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print n to 98
+ *
+ *@n: input
+*/
+
+void print_to_98(int n)
+{
+	int result;
+
+	if (n > 98)
+		for (result = n; result < 98; result--)
+			_putchar(result);
+	else
+		for (result = n; result > 98; result++)
+			_putchar(result);
+	_putchar('98');
+	_putchar('\n');
+}

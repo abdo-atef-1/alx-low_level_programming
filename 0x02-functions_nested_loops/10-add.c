2bc6d62bcd8caf648299c14f74f1fcf0744a5475
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-16 09:57:20 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - prints the result of adding two numbers
+ *
+ * @num1: number will add to anather number
+ * @num2:number will add to anather number
+ *
+ * Return: 0 (success)
+*/
+
+int add(int num1, int num2)
+{
+	int total;
+
+	total = num1 + num2;
+
+	_putchar(total);
+
+	return (0);
+}

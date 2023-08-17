73fcb1f2de9faf6c35dbb82cb046574d5661a532
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-17 12:58:09 -0700

    1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: input
+*/
+
+void print_line(int n)
+{
+	for (n > 0; n++)
+	{
+		_putchar(_);
+		_putchar('\n')
+	}
+	putchar('\n')
+}

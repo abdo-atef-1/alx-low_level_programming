95f36a464ed69c0bd911d425dd22109c955c3e56
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-17 05:33:35 -0700

    1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10 ; x++)
+		_putchar(x);
+		_putchar('\n')
+	return (0);
+}

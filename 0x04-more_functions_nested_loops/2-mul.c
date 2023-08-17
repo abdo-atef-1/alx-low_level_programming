3ba17c0e092a91d392f0115598bfb816d98962cd
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-17 04:51:54 -0700

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - prins result of multiplies two integers
+ *
+ * @a: input1
+ *
+ * @b: input2
+ *
+ * Return: sum (success)
+*/
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
+

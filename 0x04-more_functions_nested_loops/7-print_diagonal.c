7580fbd28391fde8595819fbe4f6804967645039
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-17 16:16:38 -0700

    1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal lines
+ *
+ * @n: input
+*/
+
+void print_diagonal(int n)
+{
+	int dia, space;
+
+	if (n <= 0)
+		_putchar('\n')
+	else
+	{
+		for (dia = 1; dia <= n; dia++)
+		{
+			for (space = 1; space <= dia; space++)
+				_putchar(' ');
+
+			_putchar('/');
+
+			_putchar('\n');
+		}
+	}
+}

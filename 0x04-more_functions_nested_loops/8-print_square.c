576e5fd991ee7a1e612e0949f8cae4fb2b03737c
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-17 19:41:50 -0700

    1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using #
+ *
+ * @size: size of square
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_square(int size)
+{
+	int lenth, wide;
+
+	for (lenth = 1; lenth <= size; lenth++)
+	{
+		for (wide = 1; wide <= size; colum++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}

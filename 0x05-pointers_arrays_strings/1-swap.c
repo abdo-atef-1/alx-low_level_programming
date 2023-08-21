421d568caadad4c0f9a64a104590330e7c2a77e8
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-21 13:24:04 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_ int - swaps the values of two integers
+ *
+ * @a: input value
+ *
+ * @b: input value
+ *
+*/
+
+void swap_int(int *a, int *b);
+{
+	int vod;
+
+	vod = *b;
+
+	*b = *a;
+
+	*b = vod;
+}

6db58ce777bce10077bad9af2664ba076adc6aa2
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-21 19:50:33 -0700

    2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** _puts - print a string followed by a new line to stdout 
+ *
+ * @str: is a pointer 
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}

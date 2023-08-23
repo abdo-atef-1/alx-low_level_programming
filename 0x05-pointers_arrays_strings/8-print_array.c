6262966dc7c97c2b995432d411fe198a73c7c0ef
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-22 19:54:36 -0700

    1

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array
+ *
+ * @a: string parameter input
+ * @n: string parameter input
+*/
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x != (n - 1))
+
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	}
+
+	printf("\n");
+}

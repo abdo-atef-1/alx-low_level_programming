8c268c30a77499472d83c657607a583ef6f791db
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-22 20:07:04 -0700

    1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - print out put
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: dest (success)
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = -1;
+
+	do {
+		x++;
+
+		dest[x] = src[x];
+
+
+	} while (src[i] != '\0');
+
+	return (dest);
+}

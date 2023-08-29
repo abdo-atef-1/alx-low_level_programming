236d455f85ca8e4f2ac57447afed5fea0bfbdd7c
abdo-atef-1 <abdoatef292@gmail.com>
2023-08-28 17:40:27 -0700

    2

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * * print_chessboard - function that prints the chessboard
+ * *
+ * *@a: first value -character
+ * *
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+
+	{
+
+		for (y = 0; y < 8; y++)
+		{
+			_putchar (a[x][y]);
+		}
+
+		_putchar('\n');
+
+	}
+
+}

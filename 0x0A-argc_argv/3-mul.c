5856b6369e9b4c116f875f5203e06aefe78f31c7
abdo-atef-1 <abdoatef292@gmail.com>
2023-09-02 15:39:57 -0700

    0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * * main - start of the program
+ * *
+ * * @argc: Counts the number of parameters that go into main
+ * *
+ * * @argv: Pointer of array of pointers containing strings entering main
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(int argc, char **argv)
+{
+	int i, x;
+
+	x = 0;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		x = 1;
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", i);
+	}
+	return (x);
+}

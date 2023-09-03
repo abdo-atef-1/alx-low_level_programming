2294770e2aaf5ccac81cf2694ab3546de97db5f9
abdo-atef-1 <abdoatef292@gmail.com>
2023-09-02 19:49:50 -0700

    0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  * *
  * * @argv: Pointer of array of pointers containing strings entering main
  * *
- * * Return: Always 0 (Success)* 
+ * * Return: Always 0 (Success)*
 */
 int main(int argc, char **argv)
 {

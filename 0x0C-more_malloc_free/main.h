aef3de71337e5713f5657b56d4c4fc39fe5fc014
abdo-atef-1 <abdoatef292@gmail.com>
2023-09-07 11:52:25 -0700

    done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdlib.h>
 
@@ -13,4 +13,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 
-#endif
+#endif /* MAIN_H*/

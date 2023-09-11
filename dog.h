e65aa6403f080db226f4b3f046761c30adff7193
abdo-atef-1 <abdoatef292@gmail.com>
2023-09-11 15:03:20 -0700

    1

---
+++ dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - it is a struct
+ *
+ * @name: the name of dog
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
